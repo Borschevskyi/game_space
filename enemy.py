aa93fa5fa2243cb675158e1eb2ab09f31e36b399
Borschevskyi <Borsch2556@gmail.com>
2023-05-08 10:32:08 +0300

    small corrections

--- enemy.py
+++ enemy.py
@@ -1,9 +1,10 @@
 import pygame
 
+
 class Enemy(pygame.sprite.Sprite):
     "Класс одного жулика"
     def __init__(self, screen):
-        'инициализация и начальная позиция жуликов'
+        """инициализация и начальная позиция жуликов"""
         super(Enemy, self).__init__()
         self.screen = screen
         self.image = pygame.image.load("images/enemy.png")
@@ -14,10 +15,10 @@ class Enemy(pygame.sprite.Sprite):
         self.y = float(self.rect.y)
 
     def draw(self):
-        "вывод жулика на экран"
+        """вывод жулика на экран"""
         self.screen.blit(self.image, self.rect)
 
     def update(self):
-        "перемещает жуликов"
-        self.y += 0.1
+        """перемещает жуликов"""
+        self.y += 0.12
         self.rect.y = self.y
