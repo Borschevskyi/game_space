35eb746846e190bcec39685e8d4f9a5f47b8bb13
Borschevskyi <Borsch2556@gmail.com>
2023-05-08 10:34:06 +0300

    small corrections

--- stats.py
+++ stats.py
@@ -1,11 +1,13 @@
 class Stats():
-
+    """отслеживание статистики"""
     def __init__(self):
+        """инициализирует статистику"""
         self.reset_stats()
         self.run_game = True
         with open('high_score.txt', 'r') as f:
             self.high_score = int(f.readline())
 
     def reset_stats(self):
-        self.guns_left = 3
+        """статистика, изменяющаяся во время игры"""
+        self.guns_left = 1
         self.score = 0
